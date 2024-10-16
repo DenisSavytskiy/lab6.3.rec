8acf7ba31167a7400bc80e89a96f60c6e678ecba
DenisSavytskiy <denys.savytskyi.ri.2024@lpnu.ua>
2024-10-17 02:49:44 +0300

    first commit

---
+++ AP_Lab№6.3.рек/AP_Lab№6.3.рек.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void generateArray(int a[], int size, int index = 0) {
+    if (index == size) return;
+    a[index] = rand() % 46 - 10;
+    generateArray(a, size, index + 1);
+}
+
+void Sort(int* a, const int size, int i) {
+    if (i >= size - 1) return;
+
+    int min = a[i];
+    int imin = i;
+    for (int j = i + 1; j < size; j++) {
+        if (min > a[j]) {
+            min = a[j];
+            imin = j;
+        }
+    }
+    a[imin] = a[i];
+    a[i] = min;
+
+    Sort(a, size, i + 1);
+}
+
+void Print(int* a, const int size, int i) {
+    cout << setw(4) << a[i];
+    if (i < size - 1)
+        Print(a, size, i + 1);
+    else
+        cout << endl;
+}
+
+int main() {
+    const int n = 10;
+    int a[n];
+
+    srand((unsigned)time(NULL));
+
+    generateArray(a, n);
+
+    Print(a, n, 0);
+
+    Sort(a, n, 0);
+
+    Print(a, n, 0);
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <algorithm>
+#include "../AP_Lab№6.3.рек/AP_Lab№6.3.рек.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestRecursiveSort
+{
+    TEST_CLASS(UnitTestRecursiveSort)
+    {
+    public:
+        TEST_METHOD(TestGenerateArray)
+        {
+            const int size = 10;
+            int arr[size];
+            generateArray(arr, size);
+
+            for (int i = 0; i < size; i++) {
+                Assert::IsTrue(arr[i] >= -10 && arr[i] <= 35);
+            }
+        }
+
+        TEST_METHOD(TestSort)
+        {
+            const int size = 10;
+            int arr[size] = { 9, 3, 7, 5, 8, 1, 0, 2, 4, 6 };
+            int expected[size] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Sort(arr, size, 0);
+
+            for (int i = 0; i < size; i++) {
+                Assert::AreEqual(expected[i], arr[i]);
+            }
+        }
+    };
+}
